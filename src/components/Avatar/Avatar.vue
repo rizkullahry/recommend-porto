657574d9cd9b1739e5831c973ef74e810c5ce777
m-rizkullah.r <m-rizkullah.r@bukitmakmur.com>
2025-05-20 16:35:52 +0700

    Add avatar props

---
+++ src/components/Avatar/Avatar.vue
@@ -0,0 +1,32 @@
+<template>
+
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import { AvatarProps } from './types/Avatar';
+
+const props = withDefaults(defineProps<AvatarProps>(), 
+    {
+        orientation: 'horizontal',
+        size: 'md'
+    }
+)
+
+const emits = defineEmits(['click', 'mouseover', 'mouseleave'])
+
+const showEdit = ref<boolean>(false)
+
+const sizeClass = computed(() => props.size ? `avatar-${props.size}` : '')
+const colorClass = computed(() => props.color ? `avatar-${props.color}` : '')
+const classes = computed(() => {
+    return [
+        sizeClass.value,
+        colorClass.value
+    ].filter(Boolean).join(' ')
+})
+</script>
+
+<style scoped lang="scss">
+@import './styles/avatar.scss';
+</style>
