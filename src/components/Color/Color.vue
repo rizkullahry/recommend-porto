b84ecade93375f70066ee1ed2cfdf211c9adfa38
m-rizkullah.r <m-rizkullah.r@bukitmakmur.com>
2025-05-20 10:28:41 +0700

    Refactor color props

--- src/components/Color/Color.vue
+++ src/components/Color/Color.vue
@@ -1,28 +1,22 @@
 <template>
-  <div class="color-component"
-    :class="[
-      `text-${color}-${shade}`,
-      `bg-${backgroundColor}-${backgroundShade}`
-    ]"
-  >
-    <slot></slot>
-  </div>
+  <span :class="colorClass">
+    <slot>{{ color }}</slot>
+  </span>
 </template>
 
 <script setup lang="ts">
-interface Props {
-  color?: 'primary' | 'secondary' | 'success' | 'error' | 'warning'
-  shade?: '100' | '200' | '300' | '400' | '500' | '600' | '700' | '800' | '900'
-  backgroundColor?: 'primary' | 'secondary' | 'success' | 'error' | 'warning'
-  backgroundShade?: '100' | '200' | '300' | '400' | '500' | '600' | '700' | '800' | '900'
-}
+import { computed } from 'vue';
+import type { ColorProps } from './types/Color';
 
-withDefaults(defineProps<Props>(), {
-  color: 'primary',
-  shade: '900',
-  backgroundColor: 'primary',
-  backgroundShade: '100'
-})
+const props = defineProps<ColorProps>();
+
+const colorClass = computed(() => {
+  return [
+    `text-${props.color}-${props.shade}`,
+    `bg-${props.backgroundColor}-${props.backgroundShade}`
+  ]
+});
+
 </script>
 
 <style lang="scss" scoped>
